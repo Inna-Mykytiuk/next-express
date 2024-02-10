a0a84574d70275c8e6fae4040c862204bfa0edc1
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-02-10 18:22:28 +0200

    clear project

--- types/userTypes.ts
+++
@@ -1,7 +0,0 @@
-export type userTypes = {
-  id: string;
-  name: string | null;
-  email: string | null;
-  emailVerified: Date | string | null;
-  image: string | null;
-} | null
--- utils/formatDate.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (dateTime: string) => {
-  const dateObj = new Date(dateTime); 
-  
-  const date = dateObj.getDate();
-  const month = dateObj.getMonth() + 1;
-  const year = dateObj.getFullYear();
-
-  return `${month}-${date}-${year}`;
-};
