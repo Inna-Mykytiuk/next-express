72795f09c681bac47f4853016b18497116b7d827
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-02-10 18:33:42 +0200

    firs commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import styles from "./page.module.css";
+import course from "./course.json";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <ul>
+        {course.lessons.map((lesson) => {
+          return (
+            <li key={lesson.name}>
+              <h2>{lesson.title}</h2>
+              <p>{lesson.shortSummary}</p>
+            </li>
+          );
+        })}
+      </ul>
+
+    </main>
+  );
+}
